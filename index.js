50039abbda4ab71b7797d4b3e6f08a3a470eadc1
John Brown <johnbrownsinbox@gmail.com>
2022-02-27 20:54:05 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(arrayOfStrings, string){
+    //Case insensitive
+    const matchingArrayofStrings=arrayOfStrings.filter(name=>{if(name.toLowerCase()===string.toLowerCase()){return true;}});
+    return matchingArrayofStrings;
+}
+
+function fuzzyMatch(arrayOfStrings,string){
+    const firstThreeLetters=string.substring(0,string.length);
+    console.log(firstThreeLetters);
+    return arrayOfStrings.filter(name=>{if(name.substring(0,firstThreeLetters.length)===firstThreeLetters){return true}});
+}
+
+function matchName(arrayOfObjects,suppliedString){
+    const arrayOfObjectsNamedString=arrayOfObjects.filter(objectInArray=>{if(objectInArray.name===suppliedString){
+        return true
+    }
+    else{
+
+    }
+})
+    return arrayOfObjectsNamedString
+}
